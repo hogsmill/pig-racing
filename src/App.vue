306c30cd09da67aada21b42845fbc795aeb59421
hogsmill <info@hogsmill.com>
2020-06-08 10:36:05 +0100

    fix localhost port reference

--- src/App.vue
+++ src/App.vue
@@ -353,7 +353,7 @@ export default {
     }
   },
   created() {
-    this.socket = io("http://localhost:3000");
+    this.socket = io("http://77.68.122.69:3000");
   },
   mounted() {
     this.socket.on("setRace", (data) => {
